c16936f4e8ac02fcf9bb0d12d0f0de7f6994e3b4
Honys <69396539+zHonys@users.noreply.github.com>
2023-04-06 03:23:07 -0300

    updated Shader to Silk.Net Framework

--- MagicCube/controls/Shader.cs
+++ MagicCube/controls/Shader.cs
@@ -1,111 +1,91 @@
-﻿using OpenTK.Graphics.OpenGL4;
-using OpenTK.Mathematics;
+﻿using Silk.NET.OpenGL;
+using Silk.NET.Maths;
 
-namespace MagicCube.controls
+namespace MagicCube.Controls
 {
     public class Shader : IDisposable
     {
-        public int handle;
-        private bool handleDisposed = false;
-        public Shader(string vertShaderPath, string fragShaderPath)
+        private readonly GL Gl;
+        public readonly uint Handle;
+        public Shader(GL gl, string vertShaderRelativePath, string fragShaderRelativePath)
         {
-            handle = GL.CreateProgram();
+            Gl = gl;
 
-            string currentPath = Directory.GetCurrentDirectory();
+            Handle = Gl.CreateProgram();
+            uint vert = Gl.CreateShader(ShaderType.VertexShader);
+            uint frag = Gl.CreateShader(ShaderType.FragmentShader);
 
-            string vertShaderSource = File.ReadAllText(@$"{currentPath}\{vertShaderPath}");
-            string fragShaderSource = File.ReadAllText(@$"{currentPath}\{fragShaderPath}");
+            string curPath = Directory.GetCurrentDirectory();
+            var vertSource = File.ReadAllText(Path.Join(curPath, vertShaderRelativePath));
+            var fragSource = File.ReadAllText(Path.Join(curPath, fragShaderRelativePath));
+            Gl.ShaderSource(vert, vertSource);
+            Gl.ShaderSource(frag, fragSource);
 
-            int vertShader = GL.CreateShader(ShaderType.VertexShader);
-            int fragShader = GL.CreateShader(ShaderType.FragmentShader);
+            Gl.CompileShader(vert);
+            Gl.CompileShader(frag);
 
-            GL.ShaderSource(vertShader, vertShaderSource);
-            GL.ShaderSource(fragShader, fragShaderSource);
+            Gl.AttachShader(Handle, vert);
+            Gl.AttachShader(Handle, frag);
 
-            GL.CompileShader(vertShader);
-            GL.CompileShader(fragShader);
+            Gl.LinkProgram(Handle);
+            Gl.GetShader(vert, ShaderParameterName.CompileStatus, out int vertStatus);
+            Gl.GetShader(frag, ShaderParameterName.CompileStatus, out int fragStatus);
+            Gl.GetProgram(Handle, ProgramPropertyARB.LinkStatus, out int progStatus);
 
-
-            GL.AttachShader(handle, vertShader);
-            GL.AttachShader(handle, fragShader);
-
-            GL.LinkProgram(handle);
-
-            GL.GetShader(vertShader, ShaderParameter.CompileStatus, out int vertStatus);
-            GL.GetShader(fragShader, ShaderParameter.CompileStatus, out int fragStatus);
-            GL.GetProgram(handle, GetProgramParameterName.LinkStatus, out int linkStatus);
-            if (vertStatus + fragStatus + linkStatus < 3)
+            if (vertStatus + fragStatus + progStatus < 3)
             {
-                string vertInfoLog = GL.GetShaderInfoLog(vertShader);
-                string fragInfoLog = GL.GetShaderInfoLog(fragShader);
-                string linkInfoLog = GL.GetProgramInfoLog(handle);
-
-                Console.WriteLine($"Vertice Shader Log:\n" +
-                                  $"{vertInfoLog}\n\n" +
+                Console.WriteLine($"Vertex Shader Log:\n" +
+                                  $"{Gl.GetShaderInfoLog(vert)}\n\n" +
                                   $"Fragment Shader Log:\n" +
-                                  $"{fragInfoLog}\n\n" +
-                                  $"Program Link Log:\n" +
-                                  $"{linkInfoLog}\n\n");
+                                  $"{Gl.GetShaderInfoLog(frag)}\n\n" +
+                                  $"Program Link Status Log:\n" +
+                                  $"{Gl.GetProgramInfoLog(Handle)}\n\n");
             }
 
-            // Clean UP
+            Gl.DetachShader(Handle, vert);
+            Gl.DetachShader(Handle, frag);
 
-            GL.DetachShader(handle, vertShader);
-            GL.DetachShader(handle, fragShader);
-
-            GL.DeleteShader(vertShader);
-            GL.DeleteShader(fragShader);
+            Gl.DeleteShader(vert);
+            Gl.DeleteShader(frag);
         }
-
-        public void Use()
+        public void Use() => Gl.UseProgram(Handle);
+        public unsafe void SetUniform(string uniformName, uint value)
         {
-            GL.UseProgram(handle);
+            Gl.Uniform1(GetUniformLocation(uniformName), (int)value);
         }
-
-        public int GetAttributeLocation(string attributesName)
+        public unsafe void SetUniform(string uniformName, float value)
         {
-            return GL.GetAttribLocation(handle, attributesName);
+            Gl.Uniform1(GetUniformLocation(uniformName), value);
         }
-        public int GetUniformLocation(string uniformName)
+        public unsafe void SetUniform(string uniformName, Vector3D<float> value)
         {
-            return GL.GetUniformLocation(handle, uniformName);
+            Gl.Uniform3(GetUniformLocation(uniformName), value.ToSystem());
         }
-        public static void SetUniform(int location, Matrix4 data, bool transpose = false)
+        public unsafe void SetUniform(string uniformName, Matrix4X4<float> matrix, bool transpose = false)
         {
-            GL.UniformMatrix4(location, transpose, ref data);
+            Gl.UniformMatrix4(GetUniformLocation(uniformName), 1, transpose, (float*)&matrix);
         }
-        public void SetUniform(string uniformName, Matrix4 data, bool transpose = false)
-        {
-            GL.UniformMatrix4(GL.GetUniformLocation(handle, uniformName), transpose, ref data);
-        }
-        public void SetUniform(string uniformName, int data)
-        {
-            GL.Uniform1(GL.GetUniformLocation(handle, uniformName), data);
-        }
-
-        #region IDisposable
+        public int GetUniformLocation(string uniformName) => Gl.GetUniformLocation(Handle, uniformName);
+        #region
+        private bool disposed = false;
         ~Shader()
         {
             Dispose(false);
         }
-
         public void Dispose()
         {
             Dispose(true);
             GC.SuppressFinalize(this);
         }
-
-        public void Dispose(bool disposing)
+        private void Dispose(bool disposing)
         {
-            if (!handleDisposed)
+            if (disposed) return;
+            if (!disposing)
             {
-                GL.DeleteProgram(handle);
-                handleDisposed = true;
+                // Good practice
             }
-            if (disposing)
-            {
-                handle = 0;
-            }
+            Gl.DeleteProgram(Handle);
+            disposed = true;
         }
         #endregion
     }
